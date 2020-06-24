89a237eff08299a736ce4722dc34ff117257fb05
IvanBrasilico <brasilico.ivan@gmail.com>
2020-06-24 20:02:50 -0300

    Mudanças para publicação

--- biblio/main.py
+++ biblio/main.py
@@ -2,12 +2,11 @@ import sys
 
 from flask import Flask
 from flask_admin import Admin
-from flask_admin.contrib.sqla import ModelView
 
 sys.path.append('.')
 from biblio.models import Livro, session, LivroView
 
-SECRET='secret'
+SECRET = 'secret'
 
 app = Flask(__name__)
 app.secret_key = SECRET
@@ -19,4 +18,5 @@ app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
 admin = Admin(app, name='biblio', template_mode='bootstrap3')
 admin.add_view(LivroView(Livro, session))
 
-app.run(threaded=False, debug=True)
+if __name__ == '__main__':
+    app.run(threaded=False, debug=True)
